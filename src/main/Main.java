57f2121a2c23404143f7b00898d24773af4e2fd6
Langson <langsonzhang@gmail.com>
2021-10-09 13:24:08 -0400

    completed skeleton code for entities

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(new Admin("123").getName());
+        System.out.println(new Regular("1234").getName());
     }
 }
