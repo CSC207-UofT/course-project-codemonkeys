fc03596533e8dcd32bc6141a07ae36268c63f7cf
Langson <langsonzhang@gmail.com>
2021-10-09 12:37:48 -0400

    removed test.txt

--- src/Admin.java
+++ src/Admin.java
@@ -1,6 +1,6 @@
 public class Admin extends User {
 
-    public Admin(String name){
+    public Admin(String name) {
         super(name);
     }
 }
--- src/Vote.java
+++ src/Vote.java
@@ -1,9 +1,31 @@
 public class Vote {
+    //____________________ Variables ___________________________________________________________________________________
+
     private String voter;
     private double power;
 
-    public Vote(String voter, double power){
+    //____________________ Constructors ________________________________________________________________________________
+
+    public Vote(String voter, double power) {
         this.voter = voter;
+        this.power = power;
+    }
+
+    //____________________ Getters and Setters__________________________________________________________________________
+
+    public String getVoter() {
+        return voter;
+    }
+
+    public void setVoter(String voter) {
+        this.voter = voter;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
         this.power = power;
     }
 }
