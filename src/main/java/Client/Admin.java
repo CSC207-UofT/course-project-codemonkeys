31f1a63ffb41dd21db38b980991adf9862e05d15
Chunlin_Li <2744355699@qq.com>
2021-10-17 23:00:19 -0400

    changed main branch by hand....

--- src/main/java/Client/Admin.java
+++
@@ -1,7 +0,0 @@
-package Entities;
-
-public class Admin extends User{
-    public Admin(String name) {
-        super(name);
-    }
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-import Client.Admin;
-import Client.Portfolio;
-import Client.Regular;
-import Client.User;
-import Manager.SessionManager;
-import Manager.TradeManager;
-
-import java.util.Scanner;
-
-
-public class Main {
-    public static void main(String[] args) {
-        Admin a = new Admin("A");
-        Regular user = new Regular("User 1");
-        Portfolio p1 = new Portfolio("P1");
-        Portfolio p2 = new Portfolio("P2");
-
-        SessionManager session1 = new SessionManager(1);
-        session1.setAdmin(a);
-
-        session1.addProduct(p1);
-        session1.addProduct(p2);
-        session1.display();
-
-        System.out.println("Please enter a price to buy: ");
-
-
-        Scanner sc = new Scanner(System.in);
-        double price = sc.nextDouble();
-
-        boolean success = false;
-        TradeManager trader1 = new TradeManager();
-        if (session1.tradable(user, p1)) {
-            success = trader1.buy(user, p1, price);
-        }
-        if (success) {
-            session1.remove(p1);
-        }
-=======
 
 import Entities.Asset;
 import Interfaces.YahooFinanceAPI;
@@ -63,6 +23,5 @@ public class Main {
 //            Thread.sleep(2000);
 //
 //            System.out.println(quantity + " Tesla Stock is now worth $" + value + " at price $" + price);
->>>>>>> Andrew
     }
 }
