c100140215bf52e24834270c8dc1a2aa91894977
RaymondZhang24 <yuran.zhang@mail.utoronto.ca>
2021-11-11 13:52:03 -0500

    TransactionManager class is completed not yet tested

--- src/main/java/UseCases/TransactionManager.java
+++ src/main/java/UseCases/TransactionManager.java
@@ -1,4 +1,42 @@
 package UseCases;
+import Entities.Transaction;
+import Entities.Vote;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class TransactionManager {
+    private Map<UUID, Transaction[]> transactionMap;
+
+    private TransactionManager(){ this.transactionMap = new HashMap<UUID, Transaction[]>(); }
+
+    public void creat(String from, String to, double val){
+         Transaction[] trans = Transaction.generateTransactionPair(from, to, val);
+         this.transactionMap.put(trans[0].getId(), trans);
+    }
+
+    public String viewTransaction(){
+        StringBuilder res = new StringBuilder();
+        for (Map.Entry<UUID, Transaction[]> entry : this.transactionMap.entrySet()) {
+            UUID u = entry.getKey();
+            Transaction v[] = entry.getValue();
+            for (Transaction t: v) {
+                res.append("id: ").append(u.toString()).append(", from ").append(t.getFrom_type()).
+                        append(", to: ").append(t.getTo_type()).
+                        append(", value: ").append(t.getValue());
+            }
+        }
+        return res.toString();
+    }
+
+    public void delete(Transaction trans){this.transactionMap.remove(trans.getId());}
+
+    public void delete(UUID id){this.transactionMap.remove(id);}
+
+    public Transaction[] getTransaction(UUID id){return this.transactionMap.get(id); }
+
+
+
 }
