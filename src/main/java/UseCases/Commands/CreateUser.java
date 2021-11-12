4158c42014061ead7191b70231d03afc4b13c3f8
tammyliu <tammygin.liu@mail.utoronto.ca>
2021-11-12 16:26:49 -0500

    create user

--- src/main/java/UseCases/Commands/CreateUser.java
+++ src/main/java/UseCases/Commands/CreateUser.java
@@ -1,4 +1,17 @@
 package UseCases.Commands;
 
-public class CreateUser {
+public class CreateUser implements Command{
+
+    public CreateUser() {
+    }
+
+    @Override
+    public boolean execute() {
+        return false;
+    }
+
+    @Override
+    public String help() {
+        return "this is info for the createuser command";
+    }
 }
