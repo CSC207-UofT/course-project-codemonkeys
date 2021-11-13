0a2188978589a141f2435d14818dcfc6a4f73e68
Langson <langson.zhang@mail.utoronto.ca>
2021-11-13 17:56:51 -0500

    importated raymond's TransactionManager module

--- src/main/java/Containers/Transaction.java
+++ src/main/java/Containers/Transaction.java
@@ -4,17 +4,21 @@ import Assets.Asset;
 import Identification.Identifiable;
 import Users.User;
 
+import java.util.Date;
+
 public class Transaction extends Identifiable {
 
     private final User initiator;
     private final Asset in;
     private final Asset out;
+    private final Date date;
 
     public Transaction(User initiator, Asset in, Asset out){
         super(null);
         this.initiator = initiator;
         this.in = in;
         this.out = out;
+        this.date = new Date();
     }
 
     public Asset getIn() {
