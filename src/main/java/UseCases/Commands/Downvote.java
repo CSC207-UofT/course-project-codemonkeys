7ac2cd4acd6abb699b96b414a77d4e0d44b13a1a
tammyliu <tammygin.liu@mail.utoronto.ca>
2021-11-13 16:46:24 -0500

    Basic structure finished

---
+++ src/main/java/UseCases/Commands/Downvote.java
@@ -0,0 +1,19 @@
+package UseCases.Commands;
+
+import java.util.ArrayList;
+
+public class Downvote implements Command{
+
+    public Downvote() {
+    }
+
+    @Override
+    public boolean execute(ArrayList args) {
+        return false;
+    }
+
+    @Override
+    public String help() {
+        return "this is info for the downvote command";
+    }
+}
---
+++ src/main/java/UseCases/Commands/Sell.java
@@ -0,0 +1,18 @@
+package UseCases.Commands;
+
+import java.util.ArrayList;
+
+public class Sell implements Command{
+    public Sell() {
+    }
+
+    @Override
+    public boolean execute(ArrayList args) {
+        return false;
+    }
+
+    @Override
+    public String help() {
+        return "this is info for the sell command";
+    }
+}
