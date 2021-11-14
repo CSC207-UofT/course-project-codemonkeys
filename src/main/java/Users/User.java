8d49d4ddeb04f39fdf2f0bd52a99e612e3ed716d
RaymondZhang24 <yuran.zhang@mail.utoronto.ca>
2021-11-14 02:28:26 -0500

    Updated User

--- src/main/java/Users/User.java
+++ src/main/java/Users/User.java
@@ -3,23 +3,30 @@ package Users;
 import Containers.Portfolio;
 import Identification.Identifiable;
 
+/**
+ * @Author: Vim
+ */
 public class User extends Identifiable {
 
     //____________________ Variables ___________________________________________________________________________________
 
     private String name; // Name of the User
     private Portfolio portfolio; // The personal/shadow portfolio of the User
+    private double votingPower;
+    private int securityLevel; // if the user is banned or not
 
     //____________________ Constructors ________________________________________________________________________________
 
     /**
      * Basic constructor for User
+     *
      * @param name is the name of the User
      */
     public User(String name) {
-        super(null);
         this.name = name;
         this.portfolio = new Portfolio();
+        this.votingPower = 1.0;
+        this.securityLevel = 1;
     }
 
     //____________________ Methods _____________________________________________________________________________________
@@ -32,7 +39,20 @@ public class User extends Identifiable {
         this.name = name;
     }
 
+    public void setPortfolio(Portfolio portfolio) {
+
+    }
+
     public Portfolio getPortfolio() {
         return portfolio;
     }
+
+    public void setVotingPower(double num) {
+
+    }
+    public double getVotingPower() {
+        return 0;
+    }
+
+
 }
