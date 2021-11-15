479f79bb30d9ce43b73f55eaaa8adc9250b652ba
andrewhz <andrewhz.zhang@mail.utoronto.ca>
2021-11-15 03:58:19 -0500

    GUI on edwards branch

---
+++ src/main/java/Interfaces/GraphicsPresenter/PortfolioPieChartPanel.java
@@ -0,0 +1,87 @@
+package Interfaces.GraphicsPresenter;
+
+import Assets.Asset;
+import Assets.AssetType;
+import Containers.Portfolio;
+import Managers.AssetMangers.AssetPriceHistoryManager;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.PieSectionLabelGenerator;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+
+import java.awt.*;
+import java.text.DecimalFormat;
+import java.util.UUID;
+
+class PortfolioPieChartPanel implements Panel {
+    // Generate a pie chart showing the portfolio composition
+
+    //____________________ Variables ___________________________________________________________________________________
+    Portfolio portfolio;
+
+
+    //____________________ Constructors ________________________________________________________________________________
+    public PortfolioPieChartPanel(Portfolio portfolio){
+        this.portfolio = portfolio;
+    };
+
+    public PieDataset getData() {
+        DefaultPieDataset series = new DefaultPieDataset( );
+        for (UUID id: portfolio.keySet()) {
+            String symbol = ((AssetType) portfolio.get(id).getType()).getSymbol();
+            double value = portfolio.get(id).getValue();
+            series.setValue(symbol , value);
+        }
+
+//        DefaultPieDataset series = new DefaultPieDataset( );
+//        series.setValue( "TSLA" , 2000 );
+//        series.setValue( "GOOG" , 3150  );
+//        series.setValue( "MSFT" , 400 );
+//        series.setValue( "AAPL" , 1000 );
+        return series;
+    }
+
+    public ChartPanel getPanel(int x, int y, int width, int height) {
+
+        PieDataset dataset = getData();
+        JFreeChart pieChart = ChartFactory.createPieChart("Portfolio Compsition", dataset);
+        pieChart.removeLegend();
+
+        // Plot customization
+        PiePlot plot = (PiePlot) pieChart.getPlot();
+        plot.setBackgroundPaint(Color.white);
+        plot.setOutlineVisible(false);
+        plot.setShadowPaint(null);
+        plot.setLabelShadowPaint(null);
+        plot.setLabelBackgroundPaint(Color.WHITE);
+        PieSectionLabelGenerator generator = new StandardPieSectionLabelGenerator(
+                "{0}: ${1} ({2})", new DecimalFormat("0"), new DecimalFormat("0.00%"));
+        plot.setLabelGenerator(generator);
+
+        // Calculate greyscale according to percentage
+        double max = 0.0;
+        for (int i = 0; i < dataset.getItemCount(); i++) {
+            Number value = dataset.getValue(i);
+            if ((Double) value > max) {
+                max = (Double) value;
+            }
+        }
+        for (int i = 0; i < dataset.getItemCount(); i++) {
+                plot.setSectionPaint(dataset.getKey(i), new Color(
+                        (float) (1-(double) (dataset.getValue(i)) / max),
+                        (float) (1-(double) (dataset.getValue(i)) / max),
+                        (float) (1-(double) (dataset.getValue(i)) / max)));
+            }
+
+        // Create chart panel
+        ChartPanel cp = new ChartPanel(pieChart);
+        cp.setMouseWheelEnabled(true);
+        cp.setBounds(x, y, width, height);
+        return cp;
+    }
+
+}
