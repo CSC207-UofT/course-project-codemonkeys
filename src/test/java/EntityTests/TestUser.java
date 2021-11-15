4fd40022c6a55d399be6cafdf96f9663e671c96c
Vim0315 <dupeng000413@gmail.com>
2021-11-14 19:09:24 -0500

    Test cases for users

--- src/test/java/EntityTests/TestUser.java
+++ src/test/java/EntityTests/TestUser.java
@@ -1,13 +1,34 @@
 package EntityTests;
 
+import Containers.Portfolio;
 import Users.User;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TestUser {
+    User tested_user;
+    final static String user_name = "test";
+
+    @Before
+    public void set_up() {
+        this.tested_user = new User("test");
+    }
+
     @Test(timeout = 50)
     public void testUsergetName() {
-        User user = new User("test");
-        assertEquals(user.getName(), "test");
+        assertEquals(this.tested_user.getName(), "test");
     }
+
+    @Test(timeout = 50)
+    public void testUsersetName() {
+        this.tested_user.setName("test2");
+        assertEquals(this.tested_user.getName(), "test2");
+    }
+
+    @Test(timeout = 50)
+    public void testUsergetPortfolio() {
+        Portfolio port = this.tested_user.getPortfolio();
+        assert (port.values().isEmpty());
+    }
+
 }
