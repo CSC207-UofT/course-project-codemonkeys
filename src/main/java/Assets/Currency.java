eb34de82d8312155013725b1ddc97310ffb17cf0
andrewhz <andrewhz.zhang@mail.utoronto.ca>
2021-11-14 20:28:37 -0500

    Revert "Merge pull request #18 from CSC207-UofT/Andrew-UI-Compatible"
    This reverts commit 203fba52ce5726e82b28436dc7061caf9c6155ee, reversing
    changes made to f3f7e719639030112a1256e8c2ce66d0edc3ec29.

--- src/main/java/Assets/Currency.java
+++ src/main/java/Assets/Currency.java
@@ -3,17 +3,5 @@ package Assets;
 public class Currency extends AssetType {
     public String name;
     public String symbol;
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public String getSymbol() {
-        return this.symbol;
-    }
-
     public double usdPerUnit;
-
 }
--- src/main/java/Assets/Stock.java
+++ src/main/java/Assets/Stock.java
@@ -4,26 +4,11 @@ public class Stock extends AssetType{
     public int rank;
     public String name;
     public String symbol;
-
-    public boolean liquid;
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public String getSymbol() {
-        return this.symbol;
-    }
-
     public String company;
     public double marketCap;
-
     /**
      * add more information about the stock
      */
-
 
     @Override
     public String toString() {
