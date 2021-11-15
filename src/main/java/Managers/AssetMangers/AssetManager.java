1d39e35a74002c61d1d8861fd60b464197d739b3
andrewhz <andrewhz.zhang@mail.utoronto.ca>
2021-11-14 19:52:59 -0500

    Completed AssetPriceHistoryManager

--- src/main/java/Managers/AssetMangers/AssetManager.java
+++
@@ -1,8 +0,0 @@
-package Managers.AssetMangers;
-
-/**
- * Facade class for asset management.
- */
-
-public class AssetManager {
-}
--- src/main/java/Managers/AssetMangers/AssetPriceHistoryManager.java
+++ src/main/java/Managers/AssetMangers/AssetPriceHistoryManager.java
@@ -2,28 +2,44 @@ package Managers.AssetMangers;
 
 import java.util.HashMap;
 import Assets.Asset;
+import java.util.*;
+
 
 /**
  * Manages and stores the price history of existing assets. (Queue size 25)
  */
 
 
-public class AssetPriceHistoryManager {
+public class AssetPriceHistoryManager extends HashMap<Asset, Queue<Double>>{
     //____________________ Variables ___________________________________________________________________________________
 
-    private double volume; // The volume of the asset: ex. how many shares of X stock
-    private double price; // The price per unit volume of the asset
+    private static AssetPriceHistoryManager instance;
 
     //____________________ Constructors ________________________________________________________________________________
-
-    /**
-     * Basic constructor.
-     * @param volume is how many of the asset
-     * @param price is how much $ its worth
-     * @param type holds the information about the type of asset
-     */
-
+    private AssetPriceHistoryManager(){
+        super();
+    }
 
     //____________________ Methods _____________________________________________________________________________________
+    public static AssetPriceHistoryManager getInstance() {return instance;}
 
+    public void enqueueAssetPrice(Asset asset) {
+        double price = asset.getPrice();
+
+        if (!this.containsKey(asset)){
+            Queue<Double> netQueue = new LinkedList<>();
+            this.put(asset, netQueue);
+        }
+
+        Queue<Double> theQueue = this.get(asset);
+
+        theQueue.add(price);
+
+        if (theQueue.size() > 25)
+            theQueue.remove();
+
+    }
 }
+
+
+
