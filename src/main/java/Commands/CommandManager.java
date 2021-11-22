71170d1c2ceb5947b3bbf76abed033d6cf75d60a
Langson <langson.zhang@mail.utoronto.ca>
2021-11-21 23:58:58 -0500

    Changed CommandManager: added documentation and generate() method

--- src/main/java/Commands/CommandManager.java
+++ src/main/java/Commands/CommandManager.java
@@ -1,12 +1,3 @@
-/**
- * @File: CommandManager.java
- * @Brief: Class for centralized manipulation of Commands
- *
- * @Author: Langson Zhang for CSC207 codeMonkeys
- * @Date: Nov 11 2021
- * @Version: 1.0
- */
-
 package Commands;
 
 import org.reflections.Reflections;
@@ -16,7 +7,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-
+/**
+ * This is a Class for centralized manipulation of Commands.
+ * It provides Class templates as well as Class Objects themselves.
+ * The use of reflection in this Class allows it to self-inspect for
+ * all Command subclasses.
+ *
+ * Author Langson Zhang
+ * Date: Nov 11 2021
+ * Version: 1.0
+ */
 public class CommandManager {
     private static CommandManager instance; // The instance
 
@@ -25,8 +25,18 @@ public class CommandManager {
 
     //______________________________________________ Constructors ______________________________________________________
 
+    /**
+     * Default Constructor.
+     * Scans for all subclasses of Command and creates the template list and Class list.
+     */
     private CommandManager() {
-        init(); // Initialize the class variables
+        // We use the reflection api to scan for all subtypes of Command
+        cmdClasses = new Reflections(Command.class).getSubTypesOf(Command.class);
+        cmdTemplates = new ArrayList<Command>();
+
+        for (Class c : cmdClasses){
+            cmdTemplates.add(generate(c));
+        }
     }
 
     static {
@@ -36,23 +46,22 @@ public class CommandManager {
     //_________________________________________________ Methods ________________________________________________________
 
     /**
-     * Helper method for the initialization phase
+     * Generates and returns an instance of a Command given the Class Object.
+     * @param cmdClass is the Class Object associated with [T]
+     * @param <T> is the Command subclass to be generated
+     * @returns a Command of subclass [T]
      */
-    private void init() {
-        // We use the reflection api to scan for all subtypes of Command
-        cmdClasses = new Reflections(Command.class).getSubTypesOf(Command.class);
-        cmdTemplates = new ArrayList<Command>();
-
-        // Then we try to instantiate those Classes
+    public <T extends Command> T generate(Class<T> cmdClass){
         try{
-            for (Class c : cmdClasses) {
-                cmdTemplates.add((Command) c.getConstructor().newInstance());
-            }
+            return cmdClass.getConstructor().newInstance();
         }
         catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e){
             e.printStackTrace();
         }
+        return null;
     }
+
+    //_________________________________________________ Getters and Setters ____________________________________________
 
     /**
      * Gets the singleton instance
@@ -62,11 +71,9 @@ public class CommandManager {
         return instance;
     }
 
-    /**
-     * Gets the templates
-     * @returns the templates
-     */
     public List<Command> getTemplates(){
         return this.cmdTemplates;
     }
+
+
 }
