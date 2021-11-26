bf63426f99ac0ca638f03d3990e7a11c465763d9
Langson <langson.zhang@mail.utoronto.ca>
2021-11-26 02:38:05 -0500

    Refactored Command interface to abstract class and removed execute() parameters per Tammy's suggestion.

--- src/main/java/Commands/Command.java
+++ src/main/java/Commands/Command.java
@@ -7,29 +7,43 @@ import Users.User;
 /**
  * This is the Command Interface. All communications to business logic should be done through a Command.
  *
- * Author Langson Zhang
- * Date: Nov 24 2021
- * Version: 1.0
+ * Author: Langson Zhang
+ * Date: Nov 26 2021
+ * Version: 2.0
  */
-public interface Command {
+public abstract class Command {
+
+    protected final User initiator; // who
+    protected final ClientInterface client; // where
+    protected final String[] args; // what
+
+    /**
+     * Basic constructor for a Command.
+     * @param initiator is "who" called this Command
+     * @param client is "where" the call came from
+     * @param args is "what" to do
+     */
+    public Command(User initiator, ClientInterface client, String[] args) {
+        this.initiator = initiator;
+        this.client = client;
+        this.args = args;
+    }
 
     /**
      * Executes the functionality of a Command
      * @returns true if successfully exectued, false otherwise
-     * @param client
-     * @param args
      */
-    public boolean execute(User user, ClientInterface client, String[] args);
+    public abstract boolean execute();
 
     /**
      * Gets the help string for this Command
      * @returns instructions on how to use this Command in String format
      */
-    public String help();
+    public abstract String help();
 
     /**
      * Gets the name of this Command
      * @returns a name that can be used to match an input string to this Command
      */
-    public String name();
+    public abstract String name();
 }
