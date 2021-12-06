c0aae3a4f3ea3f1e6c5fc7b6260d07f08e8f3cfc
Edward11235 <lichunlin11235@gmail.com>
2021-12-05 23:51:22 -0500

    Added main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import Interfaces.DiscordClient;
+import UseCase.Managers.PerformanceHistoryManager;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws LoginException, IOException {
+        DiscordClient discordClient = new DiscordClient();
+    }
+}
